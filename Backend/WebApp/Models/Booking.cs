eb74a19236d9c45ae45516e489ebce13672c75f2
Harshal Mutyalwar <harshalmutyalwar@gmail.com>
2024-08-05 22:01:58 +0530

    Booking Model Added

--- Backend/WebApp/Models/Booking.cs
+++ Backend/WebApp/Models/Booking.cs
@@ -1,6 +1,63 @@
-﻿namespace Hash.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace Hash.Models
 {
-	public class Booking
-	{
-	}
+    [Table("Booking")]
+    public class Booking
+    {
+        [Key]
+        [Column("BookingId")]
+        public int BookingId { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        [Column("VehicleNumber")]
+        public string VehicleNumber { get; set; }
+
+
+        [Required]
+        [Column("Date")]
+        public DateTime Date { get; set; }
+
+        [Required]
+        [Column("ParkingCostPerHour")]
+        public double ParkingCostPerHour { get; set; }
+
+
+        [Required]
+        [Column("VehicleType")]
+        public byte VehicleType { get; set; }
+
+
+        [Required]
+        [Column("StartTime")]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        [Column("EndTime")]
+        public DateTime EndTime { get; set; }
+
+        [Required]
+        [Column("TotalCost")]
+        public double TotalCost { get; set; }
+
+        [Required]
+        [Column("Slots")]
+        public int Slots { get; set; }
+
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
