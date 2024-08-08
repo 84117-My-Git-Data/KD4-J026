1f01efac560aef5b59d8fa9a9470e4f9b5494577
sima-83599 <sima11katkar@gmail.com>
2024-08-08 19:43:03 +0530

    Admin added in frontend

---
+++ frontend/src/screens/AdminLogin.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import Navbar from "../components/navbar";
+
+
+function AdminLogin() {
+
+    const navigate = useNavigate()
+    const [email, setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [isEmailEmpty, setEmailEmpty] = useState(false)
+    const [isPassowrdEmpty, setPasswordEmpty] = useState(false)
+
+    const onLogin = () => {
+        if (email.length == 0) {
+            toast.error('please enter email')
+        } else if (password.length == 0) {
+            toast.error('please enter password')
+        } else{
+            //call login API and check its status
+            // go to home screen
+            navigate('/home')
+        }
+    }
+
+
+    return (<div>
+        <Navbar/>
+        <br />
+        <br />
+        <br />
+        <div className="container mt-5 ">
+        <h2 className="page-header">Admin Login</h2>
+       
+        <div className="row">
+            <div className="col-3"></div>
+            <div className="col">
+                <div className="form">
+                    <div className="mb-3">
+                        <label htmlFor="">Email</label>
+                        <input onChange={(e)=> {
+                            if (e.target.value.length == 0) {
+                                setEmailEmpty(true)
+                            } else {
+                                setEmailEmpty(false)
+                            }
+                            setEmail(e.target.value)
+                        }}
+                        type="email" className="form-control"/>
+                        {isEmailEmpty && (
+                        <p style={{ color: 'red'}}>Email is mandatory</p>
+                        )}
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="">Password</label>
+                        <input onChange={(e)=> {
+                            if (e.target.value.length == 0) {
+                                setPasswordEmpty(true)
+                            } else {
+                                setPasswordEmpty(false)
+                            }
+                            setPassword(e.target.value)}}
+                        type="password" className="form-control"/>
+                        {isPassowrdEmpty && (
+                        <p style={{ color: 'red'}}>Password is mandatory</p>
+                        )}
+                    </div>
+                    <div className="mb-3">
+                        <button onClick={onLogin} className="btn btn-success">Login</button>
+                    </div>
+                </div>
+            </div>
+            <div className="col-3"></div>
+            </div>
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+                    
+    </div>
+
+    );
+}
+
+export default AdminLogin
