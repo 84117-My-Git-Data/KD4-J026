651475aa040d8f1a6209899872168a56cc7b20b4
kd4-hemant-sahu-83520 <rkhemant100@gmail.com>
2024-08-09 00:08:20 +0530

    Booking Request dto added

---
+++ Backend/WebApp/Dto/BookingRequestDto.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Serialization;
+using Hash.Models;
+
+namespace WebApp.Dto
+{
+    public class BookingRequestDto
+    {
+
+        public string VehicleNumber { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public byte VehicleType { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public int Slots { get; set; }
+
+        public int UserId { get; set; }
+
+        [JsonIgnore]
+        public double TotalCost { get; set; }
+
+        [JsonIgnore]
+        public double ParkingCostPerHour { get; set; }
+
+
+
+    }
+}
