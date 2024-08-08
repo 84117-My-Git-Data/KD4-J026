6a1cb6145e605612bd78269aee41677227f9c7e3
Harshal Mutyalwar <harshalmutyalwar@gmail.com>
2024-08-08 21:47:43 +0530

    Backend Floor Updated

--- Backend/WebApp/Models/Floor.cs
+++ Backend/WebApp/Models/Floor.cs
@@ -1,6 +1,29 @@
-﻿namespace Hash.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hash.Models
 {
-	public class Floor
-	{
-	}
+    [Table("Floor")]
+    public class Floor
+    {
+        [Key]
+        public int FloorId { get; set; }
+
+        [Column("FloorName")]
+        public string FloorName { get; set; }
+
+
+        [Column("SlotAvailable")]
+        public int SlotAvailable { get; set; }
+
+
+        [Column("SlotOccupied")]
+        public int SlotOccupied { get; set; }
+
+
+
+        [Column("TotalSlot")]
+        public int TotalSlot { get; set; }
+
+    }
 }
