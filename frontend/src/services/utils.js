04fd8aab50289c98ee9eaee837979209b19dcf22
KD4-amisha-84117 <vaishaligangwar1997@gmail.com>
2024-08-09 23:02:44 +0530

    added parameter error in service

--- frontend/src/services/utils.js
+++ frontend/src/services/utils.js
@@ -3,6 +3,6 @@ export function createUrl(path) {
     return `${SERVER_URL}/${path}`
 }
 
-export function createError() {
+export function createError(error) {
     return {status: 'error', error}
 }
