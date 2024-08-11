16d4f1358e863b593d401a76133216737b82f00d
KD4-amisha-84117 <vaishaligangwar1997@gmail.com>
2024-08-11 13:28:35 +0530

    changes in Admin Booking and app.js

---
+++ frontend/src/screens/AdminBookingDetails.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../components/navbar"
+
+function AdminBookingDetails() {
+    return (<div className="container-fluid">
+        <Navbar/>
+        <h2 className="page-header">Booking Details</h2>
+    </div>
+    )
+}
+
+export default AdminBookingDetails
